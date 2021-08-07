9169cc48b334d1d10c49b44c7d3422f66652e48c
fabio <dasilvadeoliveirafabio@gmail.com>
2021-08-06 21:15:19 -0300

    controller corrigido

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 use App\Http\Controllers\tarefaController2;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\controllerFoto;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
